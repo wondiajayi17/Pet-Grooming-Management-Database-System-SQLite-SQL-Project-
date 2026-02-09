b63cc963b9ea2033312872d730be7daa6b815ef3
Wonderful Ajayi <114928953+wondiajayi17@users.noreply.github.com>
2026-02-09 22:59:51 +0000

    Add files via upload

---
+++ insert_grooming.sql
@@ -0,0 +1,11 @@
+INSERT INTO GroomingInfo 
+VALUES
+("W011","Wash and Dry", 15, 10.00),
+("E899","Ear Cleaning",10,7.50),
+("N045","Nail Trim", 10, 5.00),
+("W050","Luxury Wash and Dry",25,17.50),
+("C021", "Teddy Bear Cut", 25, 30.00),
+("C034", "Breed Trim",15, 20.00),
+("X110", "Undercoat Reduction",10, 7.00),
+("C011", "Lion Cut",25, 5.00),
+("N055", "Nail trim and paint",20, 10.00);
---
+++ insert_owners.sql
@@ -0,0 +1,12 @@
+INSERT INTO Owner
+VALUES
+(765, "Mahima Raab", 543777123,"raabm@btd.com"),
+(212, "Samantha Kibson",789067123,"kibsons@yakkity.co.uk"),
+(542, "Peter Small", 123654263, "small@gsnail.com"),
+(213, "Jordan Foster", 6734321022,"FosterMJ@Yahoop.com"),
+(101, "Joe Brown", 0121543182,"Jbrown@gmaiy.com"),
+(620, "Fathima Masud", 342907652,"Jbrown@gmaiy.com"),
+(776, "Samuel Johnson", 231592833,"Johnsons@yakkity.com"),
+(452, "Colin Daniels", 247690345,"cs@aoool.com"),
+(231, "Wen Chung", 533789873, "wchung@btbt.com"),
+(398, "Penny Patel", 7684298871,"ppatel@gsnail.com");
